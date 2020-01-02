17798abdfba77315ae504743eac849ce6a192ba4
Ignand <ignatius.and@gmail.com>
2020-01-02 14:44:59 -0500

    generate project

---
+++ src/components/app.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <div>
+          logo
+        </div>
+        <div>
+          
+        </div>
+      </div>
+    );
+  }
+}
